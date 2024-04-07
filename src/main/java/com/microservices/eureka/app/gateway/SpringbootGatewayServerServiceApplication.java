5b9e190bee57933cc27ba6324d88ab59ad48428a
Iker Marin <IkerMarin@users.noreply.github.com>
2024-04-07 15:50:08 +0200

    feat: Configuration and initialization of gateway server

---
+++ src/main/java/com/microservices/eureka/app/gateway/SpringbootGatewayServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.eureka.app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGatewayServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGatewayServerServiceApplication.class, args);
+	}
+
+}
